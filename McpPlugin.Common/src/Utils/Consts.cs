7a5669a320f3f8ca4b6dc8cb5862a6c61ea75023
Ivan Murzak <Ivan.D.Murzak@gmail.com>
2025-12-26 04:27:29 -0800

    Bump version to 1.3.0 across all projects

--- McpPlugin.Common/src/Utils/Consts.cs
+++ McpPlugin.Common/src/Utils/Consts.cs
@@ -12,7 +12,7 @@ namespace com.IvanMurzak.McpPlugin.Common
     public static partial class Consts
     {
         public const string ApiVersion = "2.0.0";
-        public const string PluginVersion = "1.2.0";
+        public const string PluginVersion = "1.3.0";
 
         public static class Guid
         {
